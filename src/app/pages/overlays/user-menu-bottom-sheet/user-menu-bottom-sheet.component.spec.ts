ff168766165f6740fad622c2a53399a6d9431d95
Andrupu <47467951+andrupu@users.noreply.github.com>
2021-07-09 17:13:53 -0400

    Run Prettier and Lint

--- src/app/pages/overlays/user-menu-bottom-sheet/user-menu-bottom-sheet.component.spec.ts
+++ src/app/pages/overlays/user-menu-bottom-sheet/user-menu-bottom-sheet.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserMenuBottomSheetComponent } from './user-menu-bottom-sheet.component';
 
 describe('UserMenuBottomSheetComponent', () => {
-  let component: UserMenuBottomSheetComponent;
-  let fixture: ComponentFixture<UserMenuBottomSheetComponent>;
+    let component: UserMenuBottomSheetComponent;
+    let fixture: ComponentFixture<UserMenuBottomSheetComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UserMenuBottomSheetComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UserMenuBottomSheetComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserMenuBottomSheetComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserMenuBottomSheetComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/pages/overlays/user-menu-bottom-sheet/user-menu-bottom-sheet.component.ts
+++ src/app/pages/overlays/user-menu-bottom-sheet/user-menu-bottom-sheet.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user-menu-bottom-sheet',
-  templateUrl: './user-menu-bottom-sheet.component.html',
-  styleUrls: ['./user-menu-bottom-sheet.component.scss']
+    selector: 'app-user-menu-bottom-sheet',
+    templateUrl: './user-menu-bottom-sheet.component.html',
+    styleUrls: ['./user-menu-bottom-sheet.component.scss'],
 })
 export class UserMenuBottomSheetComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
