14ea354d8b0c4088f426866af3918f0fbee09aff
Andrupu <47467951+andrupu@users.noreply.github.com>
2021-09-03 13:34:46 -0400

    Add Account Photo and Update html & ts

--- src/app/pages/account-menu/app-bar-account-menu/app-bar-account-menu.component.ts
+++ src/app/pages/account-menu/app-bar-account-menu/app-bar-account-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { StateService } from '../../../services/state.service';
+// import { ConnectionPositionPair } from '@angular/cdk/overlay';
 
 @Component({
     selector: 'app-app-bar-account-menu',
@@ -9,6 +10,37 @@ import { StateService } from '../../../services/state.service';
 })
 export class AppBarAccountMenuComponent implements OnInit {
     isSmall: boolean;
+    open: boolean;
+    // items: items;
+
+    avatarImage="./assets/AccountPhoto";
+    menuTitle="Chima Thabani";
+    menuSubtitle="CThabani@example.com";
+
+    // let open = false;
+    // const items = [
+    //     {
+    //         title: 'Change Password',
+    //         icon: 'vpn_key',
+    //     },
+    //     {
+    //         title: 'Preferences',
+    //         icon: 'settings',
+    //     },
+    //     {
+    //         title: 'Log Out',
+    //         icon: 'logout',
+    //     },
+    // ];
+
+    // originX: select('positions.originX', ['start', 'center', 'end'], 'start');
+    // originY: select('positions.originY', ['top', 'center', 'bottom'], 'top');
+    // overlayX: select('positions.overlayX', ['start', 'center', 'end'], 'start');
+    // overlayY: select('positions.overlayY', ['top', 'center', 'bottom'], 'top');
+    // invertRTL: invertRTL,
+    // createPositions: (originX, originY, overlayX, overlayY): ConnectionPositionPair[] => [
+    //         new ConnectionPositionPair({ originX, originY }, { overlayX, overlayY }),
+    //     ],
 
     constructor(
         private readonly _drawerService: StateService,
